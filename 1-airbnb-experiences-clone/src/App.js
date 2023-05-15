526bcacfeff32e8ff22bbd15501c3963bfa2a6df
Ben Toosey <benjamin.toosey@gmail.com>
2023-05-15 22:11:25 +0100

    ADD: Titles section

--- 1-airbnb-experiences-clone/src/App.js
+++ 1-airbnb-experiences-clone/src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
 import Navbar from "./Components/Navbar"
+import Titles from "./Components/Titles"
 
 export default function App() {
   return(
-    <Navbar />
+    <div>
+      <Navbar />
+      <Titles />
+    </div>
   )
 }
